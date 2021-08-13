15a37929065fb974c3b60ea9d266dd965079be5a
Hamilton Ribeiro <htadeu@hotmail.com>
2021-08-12 23:41:15 -0300

    Update "Aula 36 - Estrutura Do While"

--- Aulas/Seção 4 - Estruturas de Controle/Aula 36 - Estrutura DO WHILE.md
+++ Aulas/Seção 4 - Estruturas de Controle/Aula 36 - Estrutura DO WHILE.md
@@ -7,4 +7,8 @@
 
 * Essa é a única estrutura que termina com **`;`**.
 * Nessa estrutura o bloco vem antes da expressão
+* O bloco é executado pelo menos uma vez
+* Somente ao final da primeira execução que testa a condição
 * Ela é uma estrutura menos usada
+* Quantidade indeterminada de repetições
+
--- CursoCSharp/CursoCSharp/EstruturasDeControle/EstruturaDoWhile.cs
+++ CursoCSharp/CursoCSharp/EstruturasDeControle/EstruturaDoWhile.cs
@@ -5,7 +5,17 @@ namespace CursoCSharp.EstruturasDeControle
     class EstruturaDoWhile
     {
         public static void Executar() {
+            string entrada;
 
+            do
+            {
+                Console.WriteLine("Qual o seu nome?");
+                entrada = Console.ReadLine();
+
+                Console.WriteLine("Seja bem-vindo {0}", entrada);
+                Console.WriteLine("Deseja continuar? (S/N)");
+                entrada = Console.ReadLine();
+            } while (entrada.ToLower() == "s");
         }
     }
 }
